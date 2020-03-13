6ab2502c4bbc9a3722858f672c4196d5fd78ddd9
MiSzostak <szostak.michael@gmail.com>
2020-03-13 22:54:39 +0100

    implement generations and mutations

--- src/Geometry.hpp
+++ src/Geometry.hpp
@@ -38,13 +38,27 @@ struct Ellipse {
     uint32_t color;
     Ellipse(Vec2u origin, int major, int minor, float angle, uint32_t color)
         : origin(origin), major(major), minor(minor), angle(angle), color(color) {};
+    void mutate() {
+        static std::default_random_engine e(std::time(nullptr));
+        static std::uniform_real_distribution<> rand_angle(-5, 5);
+        static std::uniform_int_distribution<> rand_origin(-10, 10);
+        static std::uniform_int_distribution<> rand_axes(-10, 10);
+        //static std::uniform_int_distribution<> rand_color(0, 0xFFFFFF);
+        origin.x += rand_origin(e);
+        origin.y += rand_origin(e);
+        int d1 = rand_axes(e) + major, d2 = rand_axes(e) + minor;
+        if (d1 >= 0) major = d1;
+        if (d2 >= 0) minor = d2;
+        angle = rand_angle(e);
+        //color = rand_color(e);
+    }
 };
 
 Ellipse randomEllipse(int max_width, int max_height) {
     static std::default_random_engine e(std::time(nullptr));
     static std::uniform_real_distribution<> rand_angle(-5, 5);
     static std::uniform_int_distribution<> rand_origin(0, std::max(max_width, max_height));
-    static std::uniform_int_distribution<> rand_axes(0, 100);
+    static std::uniform_int_distribution<> rand_axes(0, std::max(max_width, max_height) / 2);
     static std::uniform_int_distribution<> rand_color(0, 0xFFFFFF);
     return Ellipse(Vec2u(rand_origin(e), rand_origin(e)), rand_axes(e), rand_axes(e), rand_angle(e), rand_color(e));
 }
--- src/Main.cpp
+++ src/Main.cpp
@@ -18,7 +18,7 @@ void render(SDL_Renderer* renderer, SDL_Texture* texture, grid& buffer) {
     uint32_t* displayPtr;
     SDL_LockTexture(texture, nullptr, (void**)&displayPtr, &pitch);
     uint32_t* ptr = displayPtr;
-    for (size_t i = 0; i < buffer.size(); i++) {
+    for (size_t i = 0; i < buffer.size(); ++i) {
         std::copy(buffer[i].begin(), buffer[i].end(), ptr);
         ptr += buffer[i].size();
     }
@@ -31,8 +31,8 @@ void render(SDL_Renderer* renderer, SDL_Texture* texture, grid& buffer) {
 void drawEllipse(grid& buffer, Ellipse& e) {
     uint32_t bb = std::max(e.major, e.minor);
 
-    for (size_t y = std::max(0u, e.origin.y - bb); y < std::min((uint32_t)buffer.size(), e.origin.y + bb); y++) {
-        for (size_t x = std::max(0u, e.origin.x - bb); x < std::min((uint32_t)buffer[y].size(), e.origin.x + bb); x++) {
+    for (size_t y = std::max(0u, e.origin.y - bb); y < std::min((uint32_t)buffer.size(), e.origin.y + bb); ++y) {
+        for (size_t x = std::max(0u, e.origin.x - bb); x < std::min((uint32_t)buffer[y].size(), e.origin.x + bb); ++x) {
             int xc = (int)x - (int)e.origin.x;
             int yc = (int)y - (int)e.origin.y;
             if ((std::pow((xc * std::cos(e.angle) - yc * std::sin(e.angle)), 2) / std::pow(e.major, 2) +
@@ -41,6 +41,69 @@ void drawEllipse(grid& buffer, Ellipse& e) {
             }
         }
     }
+}
+
+void next_generation(grid& last_gen, const uint32_t* original) {
+    size_t h = last_gen.size(), w = last_gen[0].size();
+    grid new_gen = last_gen;
+    int current_mutation = 0;
+    double current_score = 0.0, last_score = 0.0;
+    bool first_hit = false;
+
+    Ellipse e = randomEllipse(w, h);
+    e.color = original[e.origin.y * w + e.origin.x];
+    Ellipse best_fit = e;
+
+    while (current_mutation < 500) {
+        uint32_t bb = std::max(e.major, e.minor);
+        for (size_t y = std::max(0u, e.origin.y - bb); y < std::min((uint32_t)new_gen.size(), e.origin.y + bb); ++y) {
+            for (size_t x = std::max(0u, e.origin.x - bb); x < std::min((uint32_t)new_gen[y].size(), e.origin.x + bb);
+                 ++x) {
+                uint32_t new_color;
+                int xc = (int)x - (int)e.origin.x;
+                int yc = (int)y - (int)e.origin.y;
+                if ((std::pow((xc * std::cos(e.angle) - yc * std::sin(e.angle)), 2) / std::pow(e.major, 2) +
+                     std::pow((xc * std::sin(e.angle) + yc * std::cos(e.angle)), 2) / std::pow(e.minor, 2)) <= 1.0) {
+                    new_color = 0xFF000000 | e.color;
+                } else {
+                    new_color = new_gen[y][x];
+                }
+
+                uint32_t og_color = original[y * w + x];
+                for (size_t i = 0; i < 3; i++) {
+                    int og_value = (og_color >> (i * 8)) & 0xFF;
+                    int new_value = (new_color >> (i * 8)) & 0xFF;
+                    int last_value = (new_gen[y][x] >> (i * 8)) & 0xFF;
+                    current_score += std::abs(og_value - new_value);
+                    last_score += std::abs(og_value - last_value);
+                }
+            }
+        }
+
+        current_score /= (bb * bb * 3);
+        last_score /= (bb * bb * 3);
+
+        if (current_score < last_score) {
+            for (size_t i = 0; i < h; ++i) std::copy(last_gen[i].begin(), last_gen[i].end(), new_gen[i].begin());
+            drawEllipse(new_gen, e);
+            best_fit = e;
+            first_hit = true;
+        }
+        current_score = 0;
+        last_score = 0;
+
+        e = best_fit;
+        e.mutate();
+        if (first_hit)
+            current_mutation++;
+        else {
+            e = randomEllipse(w, h);
+            e.color = original[e.origin.y * w + e.origin.x];
+            best_fit = e;
+        }
+    }
+
+    for (size_t i = 0; i < h; ++i) std::copy(new_gen[i].begin(), new_gen[i].end(), last_gen[i].begin());
 }
 
 int main(int argc, char** argv) {
@@ -59,6 +122,15 @@ int main(int argc, char** argv) {
     SDL_Surface* surface = IMG_Load(file_path.c_str());
     if (surface == nullptr) {
         printf("Failed to load image %s\n", file_path.c_str());
+        return 1;
+    }
+    if (surface->format->format != SDL_PIXELFORMAT_RGBA32) {
+        SDL_PixelFormat* format = SDL_AllocFormat(SDL_PIXELFORMAT_RGBA32);
+        surface = SDL_ConvertSurface(surface, format, 0);
+        SDL_FreeFormat(format);
+    }
+    if (surface == nullptr) {
+        printf("Failed to convert image %s to RGBA32\n", file_path.c_str());
         return 1;
     }
 
@@ -94,12 +166,8 @@ int main(int argc, char** argv) {
 
     grid buffer(h, std::vector<uint32_t>(w, 0xFFFFFFFF));
 
-    for (size_t i=0; i<100; i++) {
-        Ellipse test(randomEllipse(w, h));
-        drawEllipse(buffer, test);
-    }
-
-    bool done = false;
+    bool done = false, stop_evo = false;
+    size_t ctr = 0;
     while (!done) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
@@ -107,11 +175,15 @@ int main(int argc, char** argv) {
             if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE &&
                 event.window.windowID == SDL_GetWindowID(window))
                 done = true;
+            if (event.type == SDL_KEYUP && event.key.keysym.scancode == SDL_SCANCODE_Q) stop_evo = true;
         }
 
-        // SDL_RenderClear(renderer);
-        // SDL_RenderCopy(renderer, texture, nullptr, nullptr);
-        // SDL_RenderPresent(renderer);
+        if (!stop_evo && ctr < 1000) {
+            next_generation(buffer, reinterpret_cast<uint32_t*>(surface->pixels));
+            printf("Generation #%lu\n", ctr);
+            ctr++;
+        }
+
         render(renderer, texture, buffer);
     }
 
@@ -119,8 +191,8 @@ int main(int argc, char** argv) {
     SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
-    IMG_Quit();
     SDL_Quit();
+    IMG_Quit();
 
     return 0;
 }
